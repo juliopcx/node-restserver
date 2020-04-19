46f097df48d0b2323f318890cc773e6c91093c12
Julio <juliopcx@gmail.com>
2020-04-19 14:27:54 +0200

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,50 @@
+require('./config/config');
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function(req, res) {
+
+    res.json('get Usuario');
+})
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+
+            ok: false,
+            mensaje: 'el nombre es necesario'
+
+        });
+    }
+
+    res.json({ body });
+})
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = request.params.id;
+
+    console.log({
+        id,
+    })
+
+})
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+})
+
+app.listen(process.env.PORT, () => {
+
+    console.log('escuchando el puerto', process.env.PORT)
+
+})
